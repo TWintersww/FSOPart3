de7a7247435aec10651eaa5ec33cec621fc677f5
TWintersww <134812478+TWintersww@users.noreply.github.com>
2023-10-23 17:54:35 -0700

    Finish 3.8

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {
+    skip: function(req, res) { return req.method === 'POST'}
+}))
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  }, {skip: function(req, res) { return req.method !== 'POST'}}))
 
 let persons = [
     { 
